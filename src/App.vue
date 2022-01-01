81bee93f7bf2a785088eb6ff8b21b840dcb68a2a
Gankerjack <sebastiaancrisan@gmail.com>
2022-01-01 16:21:06 +0100

    test methods buttons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="app">Hello world</div>
+  <div class="app">
+    <p>Hello {{ name }} - {{ age }}</p>
+    <button @click="changeName('Gankerjack')">change name</button>
+    <button @click="changeAge(28)">change age</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +12,22 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
+  data() {
+    return {
+      name: "Gaiseric",
+      age: 27 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string): string {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number | string): string | number {
+      this.age = age;
+      return age;
+    },
+  },
 });
 </script>
 
